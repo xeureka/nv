b9226b4e7a4005518b7be8c82047adabfa9a069c
xeureka <156827630+xeureka@users.noreply.github.com>
2026-03-01 13:30:39 +0300

    change the defualt spacing of 2 space to 1 tab or 4 spaces

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,3 +12,9 @@ vim.cmd([[let &t_Cs = "\e[60m"]])
 vim.cmd([[let &t_Ce = "\e[24m"]])
 vim.opt.termguicolors = true
 vim.g.lazyvim_picker = "telescope"
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
